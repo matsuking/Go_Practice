60417c5b2b7f67d8e24ffbafcdb0e1c79f3f9b69
github user name <matsuking0428@gmail.com>
2023-03-19 12:36:33 +0900

    save

---
+++ main_pointer.go
@@ -0,0 +1,100 @@
+package main
+
+import (
+	"fmt"
+	"unsafe"
+)
+
+const secret = "abc"
+
+type Os int
+
+const (
+	// 上から順に連番
+	Mac Os = iota + 1
+	Windows
+	Linux
+)
+
+var (
+	// i=0, s="", b=falseで初期化
+	i int
+	s string
+	b bool
+)
+
+func main() {
+	// fmt.Println("hello world.")
+	// sl := []int{1, 2, 3}
+	// if len(sl) > 2 {
+	// 	fmt.Println("unreachable code")
+	// }
+
+	// 	godotenv.Load()
+	// 	fmt.Println(os.Getenv("GO_ENV"))
+	// 	fmt.Println(calculator.Offset)
+	// 	fmt.Println(calculator.Sum(1, 2))
+	// 	fmt.Println(calculator.Multiply(1, 2))
+
+	var i int = 2
+	fmt.Println(i)
+
+	l := 1
+	ui := uint16(2)
+	fmt.Println(l)
+	// %vは値をそのまま、%Tはデータ型を表示
+	fmt.Printf("i: %v %T\n", i, i)
+	fmt.Printf("i: %[1]v %[1]T ui: %[2]v %[2]T\n", l, ui)
+
+	f := 1.23456
+	s := "hello"
+	b := true
+	fmt.Printf("f: %[1]v %[1]T\n", f)
+	fmt.Printf("s: %[1]v %[1]T\n", s)
+	fmt.Printf("b: %[1]v %[1]T\n", b)
+
+	pi, title := 3.14, "GO"
+	fmt.Printf("pi: %v title: %v\n", pi, title)
+
+	fmt.Printf("Mac: %v Windows: %v Linux: %v\n", Mac, Windows, Linux)
+
+	var ui1 uint16
+	// ui1のaddressを表示
+	fmt.Printf("memory address of ui1: %p\n", &ui1)
+
+	var ui2 uint16
+	fmt.Printf("memory address of ui2: %p\n", &ui2)
+	var p1 *uint16
+	fmt.Printf("value of p1: %v\n", p1)
+	p1 = &ui1
+	fmt.Printf("value of p1: %v\n", p1)
+	fmt.Printf("size of p1: %d[bytes]\n", unsafe.Sizeof(p1))
+	fmt.Printf("memory address of p1: %p\n", &p1)
+	fmt.Printf("memory address of ui1(dereference): %v\n", *p1)
+
+	*p1 = 1
+	fmt.Printf("value of ui1: %v\n", ui1)
+
+	var pp1 **uint16 = &p1
+	fmt.Printf("value of pp1: %v\n", pp1)
+	fmt.Printf("memory address of pp1: %p\n", &pp1)
+	fmt.Printf("size of pp1: %d[bytes]\n", unsafe.Sizeof(pp1))
+	fmt.Printf("value of p1(dereference): %v\n", *pp1)
+	fmt.Printf("value of ui1(dereference): %v\n", **pp1)
+	**pp1 = 10
+	fmt.Printf("value of ui1: %v\n", ui1)
+
+	ok, result := true, "A"
+	fmt.Printf("memory address of result: %p\n", &result)
+	if ok {
+		// このスコープ内だけで値の置き換え。 result = "B"とすると、値の置き換えが行われる。
+		// 最初に定義したresultとは別のメモリー領域で定義される。
+		result := "B"
+		fmt.Printf("memory address of result: %p\n", &result)
+		println(result)
+	} else {
+		result := "C"
+		println(result)
+	}
+	println(result)
+}
---
+++ main_struct_receiver.go
@@ -0,0 +1,56 @@
+package main
+
+import (
+	"fmt"
+	"unsafe"
+)
+
+// 構造体はstructで作れる
+type Task struct {
+	Title    string
+	Estimate int
+}
+
+func main() {
+	task1 := Task{
+		Title:    "Learn Golang",
+		Estimate: 3,
+	}
+	task1.Title = "learning Go"
+	// %+[1]vで出力すると、field名も出力してくれる。
+	fmt.Printf("%[1]T %+[1]v %v\n", task1, task1.Title)
+
+	var task2 Task = task1
+	task2.Title = "new"
+	fmt.Printf("task1: %v task2: %v\n", task1.Title, task2.Title)
+
+	// &をつけることで先頭アドレスを取得, task1pはポインタ変数, *task1pは構造体の中身を出力
+	task1p := &Task{
+		Title:    "Learn concurrency",
+		Estimate: 2,
+	}
+	fmt.Printf("task1p: %T %+v %v\n", task1p, *task1p, unsafe.Sizeof(task1p))
+	(*&task1p).Title = "Changed"
+	fmt.Printf("task1p: %+v\n", *task1p)
+
+	var task2p *Task = task1p
+	task2p.Title = "Changed by Task2"
+	fmt.Printf("task1: %+v\n", *task1p)
+	fmt.Printf("task2: %+v\n", *task2p)
+
+	task1.extendEstimate()
+	fmt.Printf("task1 value receiver: %+v\n", task1.Estimate)
+
+	(&task1).extendEstimatePointer()
+	fmt.Printf("task1 value receiver: %+v\n", task1.Estimate)
+}
+
+// 値は変わらない。
+func (task Task) extendEstimate() {
+	task.Estimate += 10
+}
+
+// 値は変わる
+func (taskp *Task) extendEstimatePointer() {
+	taskp.Estimate += 10
+}
